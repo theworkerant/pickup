33ba01635c117247a30c96520c9415d1bf6c9495
Graham Powrie <graham@fathom.digital>
2020-05-03 16:32:23 -0600

    asset_path fix, add user model

---
+++ db/migrate/20200503223019_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :email
+      t.string :username
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
