27e3cb5c0f9d872edc8bcd7613d081379b461eff
Graham Powrie <graham@fathom.digital>
2020-05-08 14:44:06 -0600

    basic matches, games, with slot reservations and a test

---
+++ db/migrate/20200508210605_create_reservations.rb
@@ -0,0 +1,10 @@
+class CreateReservations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reservations do |t|
+      t.belongs_to :match
+      t.belongs_to :user
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/models/match_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Match, type: :model do
+
+  it "Match worksv" do
+    game = Game.create(name: "Dodgeball")
+    kate = User.create(username: "kate")
+    white = User.create(username: "white")
+    vince = User.create(username: "vince")
+    match = Match.create(host: kate, slots: 2)
+
+    # Kate joins automagically
+    expect(match.slots_remaining).to eq(1)
+
+    match.reserve(white)
+    expect(match.slots_remaining).to eq(0)
+
+    match.relinquish(kate)
+    expect(match.slots_remaining).to eq(1)
+
+    match.reserve(vince)
+    expect(match.slots_remaining).to eq(0)
+  end
+end
