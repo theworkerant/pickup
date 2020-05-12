cde9acd5438d239c78ecb6f79f2c0994d74fd18f
Graham Powrie <graham@fathom.digital>
2020-05-12 10:52:24 -0600

    more polish on embed formatting

--- app/models/match.rb
+++ app/models/match.rb
@@ -20,19 +20,30 @@ class Match < ApplicationRecord
     count < 0 ? 0 : count
   end
 
+  def formatted_start
+    self.start_time.strftime("%I:%M %p")
+  end
+  def formatted_end
+    (self.start_time + self.duration.minutes).strftime("%I:%M %p")
+  end
+
+  def formatted_time
+    "#{formatted_start} / #{formatted_end} Eastern"
+  end
+
   def announce
     match = self
     embed = Discord::Embed.new do
       title("#{match.host.username} wants to play #{match.game.name}")
       description(match.description)
       author(name: match.host.username, icon_url: match.host.picture)
-      add_field(name: "Slots", value: match.slots)
-      add_field(name: "Start", value: match.start_time.strftime("%I:%M %p Eastern"))
-      add_field(name: "End", value: (match.start_time + match.duration.minutes).strftime("%I:%M %p Eastern"))
-      add_field(name: "Queue Up!", value: "[click to reserve](https://fathompickup.herokuapp.com)")
-      image(url: ENV["HOST_URL"] + ActionController::Base.helpers.asset_url("games/#{match.game.slug}.webp"))
+      add_field(name: "Slots", value: "#{match.host.username} + **#{match.slots - 1} more**")
+      add_field(name: "Time", value: match.formatted_time)
+      add_field(name: "I'll Play!", value: "[click to reserve](https://fathompickup.herokuapp.com)")
+      thumbnail(url: ENV["HOST_URL"] + ActionController::Base.helpers.asset_url("games/#{match.game.slug}.webp"))
+      # image(url: ENV["HOST_URL"] + ActionController::Base.helpers.asset_url("games/#{match.game.slug}.webp"))
       timestamp(DateTime.now)
-      footer(text: "Fathom Pickup :: #{ENV['HOST_URL']}")
+      footer(text: "pickup.fathom.digital")
     end
 
     Discord::Notifier.message(embed)
